06b7fea56a62f93056c5e368bab0aed2444e8b0d
michaels <masouffront@gmail.com>
2016-04-07 14:02:16 -0600

    Added minor losses and water temperature inputs

--- tethysapp/hydropower/controllers.py
+++ tethysapp/hydropower/controllers.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from tethys_sdk.gizmos import TableView
-from tethys_sdk.gizmos import SelectInput
-from tethys_sdk.gizmos import Button
+from tethys_sdk.gizmos import TableView, TextInput, SelectInput, Button
 
 from math import pi
 from math import log10
@@ -34,6 +32,11 @@ def home(request):
                                    initial=['Commercial Steel'],
                                    original=False)
 
+    text_input = TextInput(display_text='Enter Water Temperature',
+                           name='inputAmount',
+                           placeholder='20.00',
+                           append=unicode(u'\u00b0' + 'C'))
+
     input_tbv = TableView(column_names=('Input', 'Value', 'Units'),
                           rows=[('Length', 739, '[ M ]'),
                                 ('Diameter', 1.5, '[ M ]'),
@@ -41,10 +44,50 @@ def home(request):
                                 ('Elevation Head', 135, '[ M ]')],
                           hover=True,
                           striped=True,
-                          bordered=False,
+                          bordered=True,
                           condensed=True,
                           editable_columns=(False, 'valueInput'),
-                          row_ids=[21, 25, 31])
+                          row_ids=[0 , 1, 2, 3])
+
+    bends_tbv = TableView(column_names=('Bends', 'Count'),
+                          rows=[('90 Smooth (Flanged)', 0),
+                                ('90 Smooth (Threaded)', 0),
+                                ('90 Miter', 0),
+                                ('45 Threaded Elbow', 1),
+                                ('Threaded Union', 121)],
+                          hover=True,
+                          striped=True,
+                          bordered=True,
+                          condensed=True,
+                          editable_columns=(False, 'BCountInput'),
+                          row_ids=[0, 1, 2, 3, 4])
+
+    inlets_tbv = TableView(column_names=('Inlets', 'Count'),
+                          rows=[('Reentrant', 0),
+                                ('Sharp Edge', 1),
+                                ('Well-Rounded', 0),
+                                ('Slightly-Rounded', 0)],
+                          hover=True,
+                          striped=True,
+                          bordered=True,
+                          condensed=True,
+                          editable_columns=(False, 'ICountInput'),
+                          row_ids=[0, 1, 2, 3])
+
+    exits_tbv = TableView(column_names=('Exit', 'Count'),
+                          rows=[('Reentrant (Turb)', 0),
+                                ('Sharp Edge (Turb)', 1),
+                                ('Rounded (Turb)', 0),
+                                ('Gradual Contraction', 0),
+                                ('30 Degree', 0),
+                                ('45 Degree', 0),
+                                ('60 Degree', 0)],
+                          hover=True,
+                          striped=True,
+                          bordered=True,
+                          condensed=True,
+                          editable_columns=(False, 'ECountInput'),
+                          row_ids=[0, 1, 2, 3, 4, 5, 6])
 
     submit_button = Button(
                     display_text='Calculate Capacity',
@@ -55,7 +98,11 @@ def home(request):
 
     context = {
         'materialDropdown': materialDropdown,
+        'text_input': text_input,
         'input_tbv': input_tbv,
+        'bends_tbv': bends_tbv,
+        'inlets_tbv': inlets_tbv,
+        'exits_tbv': exits_tbv,
         'submit_button': submit_button
     }
 
@@ -71,8 +118,8 @@ def calculate_capacity(request):
     if request.POST and 'submit' in request.POST:
         print request.POST
         pipeMaterial = float(request.POST['materialDropdown'])
-        diameter = float(request.POST['valueInput25'])
-        flow = float(request.POST['valueInput31'])
+        diameter = float(request.POST['valueInput1'])
+        flow = float(request.POST['valueInput2'])
         density = 998
         kinViscosity = 0.00000112
         turbineEfficiency = 0.53
